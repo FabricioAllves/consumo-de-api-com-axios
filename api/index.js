59f9ef885f0c353d6b63413651b175792525fd23
fabricio-26 <fabricioohh@gmail.com>
2022-10-15 17:44:17 -0300

    App.use( => userRouter)

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const connection = require("./database/database")
 const gameRouter = require('./router/gamesRouter')
+const userRouter= require('./router/usersRoutes')
 const bodyParser = require('body-parser');
 const cors = require("cors");
 
@@ -15,6 +16,7 @@ connection.authenticate().then(() => {console.log("Banco conectado com sucesso!!
 .catch(() => {console.error("Banco não conectado!")})
 
 app.use('/', express.json(), gameRouter)
+app.use('/', express.json(), userRouter)
 
 const PORT = 3333;
 app.listen(PORT, () => {
