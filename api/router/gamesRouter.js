e22847a19381d2a949eab2aa8e91c7f10323880f
fabricio-26 <fabricioohh@gmail.com>
2022-10-16 19:22:40 -0300

    Update

--- api/router/gamesRouter.js
+++ api/router/gamesRouter.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router()
 const gameController = require('../controller/gameController')
+const auth = require('../middleware/auth')
 
-router.get('/listAll', gameController.listAll)
+router.get('/listAll', auth, gameController.listAll)
 router.get('/search/:id', gameController.searchId)
-router.post('/createGame', gameController.createGame)
-router.put('/update/:id', gameController.updateId)
-router.delete('/remove/:id', gameController.deleteId)
+router.post('/createGame', auth, gameController.createGame)
+router.put('/update/:id', auth, gameController.updateId)
+router.delete('/remove/:id', auth, gameController.deleteId)
 
 module.exports = router
